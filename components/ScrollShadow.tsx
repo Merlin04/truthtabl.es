f725de5e6e7a32ea3281cd11ffe6c92796b32dce
Benjamin Smith <Merlin04@users.noreply.github.com>
2021-10-18 16:37:09 +0000

    Add scroll shadows

---
+++ components/ScrollShadow.tsx
@@ -0,0 +1,27 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+
+if(typeof window !== "undefined") {
+    require("../node_modules/scroll-shadow-element/scroll-shadow-element");
+}
+
+function ScrollShadowComponentWrapper(props: {
+    className: string,
+    children: React.ReactNode
+}) {
+    return (
+        //@ts-expect-error - TypeScript doesn't know about the custom element
+        <scroll-shadow class={props.className}>{props.children}</scroll-shadow>
+    );
+}
+
+export default function ScrollShadow(props: BoxProps) {
+    const { children, ...otherProps } = props;
+
+    return (
+        <Box as={ScrollShadowComponentWrapper} {...otherProps}>
+            <Box overflow="auto">
+                {children}
+            </Box>
+        </Box>
+    )
+}

159da2d8b2523ba201d28879353a8bede7850653
Merlin04 <merlin04@users.noreply.github.com>
2022-02-01 17:24:51 +0000

    Fix operator order

--- next.config.js
+++ next.config.js
@@ -3,5 +3,25 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/editor",
+  //       destination: "https://ohmjs.org/editor/"
+  //     },
+  //     {
+  //       source: "/third_party/:path*",
+  //       destination: "https://ohmjs.org/editor/third_party/:path*"
+  //     },
+  //     {
+  //       source: "/style/:path*",
+  //       destination: "https://ohmjs.org/editor/style/:path*"
+  //     },
+  //     {
+  //       source: "/assets/:path*",
+  //       destination: "https://ohmjs.org/editor/assets/:path*"
+  //     }
+  //   ];
+  // }
 }
